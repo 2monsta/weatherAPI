5e049b9854cb358baf732206005777e2384f15de
Yunf.li37@gmail.com <Yunf.li37@gmail.com>
2017-10-12 14:05:26 -0400

    add ignore file

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(document).ready(()=>{
+	
 	$("#weather-form").submit((event)=>{
-		console.log("sanity check")
+		event.preventDefault();
+		var zipCode = $("#zip-code").val();
+
+		var weatherURL =`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&appid=${apiKey}`
+		console.log(weatherURL);
+		$.getJSON(weatherURL, function(weatherData){
+			console.log(weatherData);
+		});
 	})
 })
